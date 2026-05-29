9f1be62511b588ac17c644d538b9639ed4ed8cc2
ThanhHuongK19 <daohuong030303@gmail.com>
2026-05-29 16:49:52 +0700

    wrote repositories

---
+++ Repositories/Implementations/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObjects.Models;
+using DataAccessObjects;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interfaces;
+
+namespace Repositories.Implementations
+{
+    public class CustomerRepository : ICustomerRepository 
+    {
+        private readonly RestaurantSystemDbContext _context;
+
+        public CustomerRepository(RestaurantSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<Customer>> GetAllCustomersAsync(
+            CancellationToken ct = default)
+        {
+            return await _context.Customers
+                .AsNoTracking()
+                .OrderBy(c => c.CustomerId)
+                .ToListAsync(ct);
+        }
+
+        public async Task<Customer?> GetCustomerByIdAsync(
+            long id,
+            CancellationToken ct = default)
+        {
+            return await _context.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.CustomerId == id, ct);
+        }
+    }
+}
---
+++ Repositories/Implementations/MenuCategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿
+using BusinessObjects.Models;
+using DataAccessObjects;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interfaces;
+
+namespace Repositories.Implementations
+{
+    public class MenuCategoryRepository : IMenuCategoryRepository
+    {
+        private readonly RestaurantSystemDbContext _context;
+
+        public MenuCategoryRepository(RestaurantSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<MenuCategory>> GetAllMenuCategoriesAsync(
+            CancellationToken ct = default)
+        {
+            return await _context.MenuCategories
+                .AsNoTracking()
+                .OrderBy(c => c.CategoryId)
+                .ToListAsync(ct);
+        }
+
+        public async Task<MenuCategory?> GetMenuCategoryByIdAsync(
+            long id,
+            CancellationToken ct = default)
+        {
+            return await _context.MenuCategories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.CategoryId == id, ct);
+        }
+    }
+}

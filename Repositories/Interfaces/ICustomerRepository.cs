b6b842bb39298ecfe778e0dce0630b59351ea75f
ThanhHuongK19 <daohuong030303@gmail.com>
2026-06-03 21:37:30 +0700

    Update repository interface

--- Repositories/Interfaces/ICustomerRepository.cs
+++ Repositories/Interfaces/ICustomerRepository.cs
@@ -1,11 +1,10 @@
 ﻿using BusinessObjects.Models;
-using System.Reflection.Metadata;
 
 namespace Repositories.Interfaces
 {
     public interface ICustomerRepository
     {
         Task<IReadOnlyList<Customer>> GetAllCustomersAsync(CancellationToken ct = default);
-        Task<Customer?> GetCustomerByIdAsync(int id, CancellationToken ct = default);
+        Task<Customer?> GetCustomerByIdAsync(long id, CancellationToken ct = default);
     }
 }
--- Repositories/Interfaces/IMenuCategoryRepository.cs
+++ Repositories/Interfaces/IMenuCategoryRepository.cs
@@ -6,6 +6,6 @@ namespace Repositories.Interfaces
     public interface IMenuCategoryRepository
     {
         Task<IReadOnlyList<MenuCategory>> GetAllMenuCategoriesAsync(CancellationToken ct = default);
-        Task<MenuCategory?> GetMenuCategoryByIdAsync(int id, CancellationToken ct = default);
+        Task<MenuCategory?> GetMenuCategoryByIdAsync(long id, CancellationToken ct = default);
     }
 }

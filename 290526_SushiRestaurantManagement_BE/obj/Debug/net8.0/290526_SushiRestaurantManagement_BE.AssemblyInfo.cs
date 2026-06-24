991ea94235c51558306e885565ed1f64fbd360c0
nhngchau <nnchau3305@gmail.com>
2026-06-24 10:15:39 +0700

    Update UI Cart + Menu

--- 290526_SushiRestaurantManagement_BE/obj/Debug/net8.0/290526_SushiRestaurantManagement_BE.AssemblyInfo.cs
+++ 290526_SushiRestaurantManagement_BE/obj/Debug/net8.0/290526_SushiRestaurantManagement_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("290526_SushiRestaurantManagement_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1d2532c99002cde2523ca0f749bdec40bbd912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79694c5361e1900a632c5e8dcccc94a0fb02e2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("290526_SushiRestaurantManagement_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("290526_SushiRestaurantManagement_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
